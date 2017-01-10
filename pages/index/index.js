7d520709d9f8d528512e51a0e24de9ef8e218120
brandonxiang <1542453460@qq.com>
2017-01-10 14:58:02 +0800

    vote message

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,29 +1,50 @@
 //index.js
 //获取应用实例
+var app = getApp()
 Page({
   data: {
     motto: 'Hello World',
     userInfo: {},
     //请点击你认为比较帅的头像
-    description:'请点击你认为比较帅的头像',
-    bluebutterfly:{
-      name:'bluebutterfly',
-      src:'../../asset/mochan.jpg',
-      score:{}
+    description: '请点击你认为比较帅的头像',
+    bluebutterfly: {
+      name: 'bluebutterfly',
+      src: '../../asset/mochan.jpg',
+      score: 100
     },
-    me:{
-      name:'me',
-      src:'../../asset/melonpi.jpg',
-      score:{}
-    }
+    me: {
+      name: 'me',
+      src: '../../asset/melonpi.jpg',
+      score:101
+    },
+    voted:true
   },
-  onClick: function(e) {
+  onClick: function (e) {
     console.log(e)
+    // app.getUserInfo(function(userinfo,rawData,signature,encryptedData,iv){
+    //   console.log(userinfo)
+    //   console.log(rawData)
+    //   console.log(signature)
+    //   console.log(encryptedData)
+    //   console.log(iv)
+    // })
+    wx.getUserInfo({
+      success: function(res){
+        // success
+        console.log(res)
+      },
+      fail: function() {
+        // fail
+      },
+      complete: function() {
+        // complete
+      }
+    })
   },
   onLoad: function () {
     console.log('onLoad')
     var that = this
 
-   
+
   }
 })
---
+++ utils/app.js
@@ -0,0 +1,26 @@
+const host = 'localhost:5000/'
+
+const wxRequest = (params, url) => {
+  wx.showToast({
+    title: '加载中',
+    icon: 'loading'
+  })
+  wx.request({
+    url: url,
+    method: params.method || 'GET',
+    data: params.data || {},
+    header: {
+      'Content-Type': 'application/json'
+    },
+    success: (res) => {
+      params.success && params.success(res)
+      wx.hideToast()
+    },
+    fail: (res) => {
+      params.fail && params.fail(res)
+    },
+    complete: (res) => {
+      params.complete && params.complete(res)
+    }
+  })
+}
